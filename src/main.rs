56582e7821cc86d7998006d424a0d74ddf55bcea
Kennet Mathisen <kennetam02@gmail.com>
2021-11-18 21:14:27 +0100

    finally! You can now run .bf files useing the run sub command followed by the path to the bf file.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-fn main() {
-    // let prog = "<<<>>><++--..,,[[]]] This is a comment";
-    let _prog = ">><<+>+>+>[[].]";
+use std::env;
+use std::fs;
 
-    brain_rust::runtime::Runtime::new(_prog).run()
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let sub_command: String = match args.get(1) {
+        Some(v) => v.to_owned(),
+        None => String::from("")
+    };
+    match sub_command.as_str() {
+        "run" => {
+            let path = &args[2];
+            let program_file = fs::read_to_string(path);
+            match program_file {
+                Ok(prog) => brain_rust::runtime::Runtime::new(&prog).run(),
+                Err(_) => println!("ERROR: Invalid path: {}", path),
+            }
+        }
+        _ => {
+            println!("Invalid sub command!\n\nAll valid sub commands:\n\t run [PATH]")
+        },
+    }
 }
+
