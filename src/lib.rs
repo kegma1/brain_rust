0346323128eb4215cbd47c7a30a456031d4b137a
Kennet Mathisen <kennetam02@gmail.com>
2021-12-05 10:18:01 +0100

    Implemented fmt::Display to Token enum

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,18 @@ pub enum Token {
     OpenBrk(usize),
     ClosedBrk(usize),
 }
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Inc(x) => write!(f, "+ {}\n", x),
+            Token::Dec(x) => write!(f, "- {}\n", x),
+            Token::LMov(x) => write!(f, "< {}\n", x),
+            Token::RMov(x) => write!(f, "> {}\n", x),
+            Token::OutStd => write!(f, ".\n"),
+            Token::InStd => write!(f, ",\n"),
+            Token::OpenBrk(x) => write!(f, "[ {}\n", x),
+            Token::ClosedBrk(x) => write!(f, "] {}\n", x),
+        }
+    }
+}
