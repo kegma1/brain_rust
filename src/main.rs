d3396951bb6f5e683e8bbe7fb946315adc5e369d
Kennet Mathisen <kennetam02@gmail.com>
2021-12-05 10:22:10 +0100

    compile to rusty brainfuck(rbf)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,7 +10,7 @@ fn main() {
         None => String::from("")
     };
     match sub_command.as_str() {
-        "run" => {
+        "run" | "r" => {
             let path = &args[2];
             let program_file = fs::read_to_string(path);
             match program_file {
@@ -16,8 +18,24 @@ fn main() {
                 Err(_) => println!("ERROR: Invalid path: {}", path),
             }
         }
+        "compile" | "c" => {
+            let path = &args[2];
+            let program_file = fs::read_to_string(path);
+            match program_file {
+                Ok(prog) => {
+                    let file_name = &path[0..path.len() - 3];
+                    let compiled_prog = brain_rust::parser::parse(&prog);
+                    let mut output = File::create(format!("{}.rbf", file_name)).expect("Was unable to create file.");
+                    for i in &compiled_prog {
+                        output.write_all(format!("{}", i).as_bytes()).expect("Was unable to write data to file.");
+                    }
+
+                },
+                Err(_) => println!("ERROR: Invalid path: {}", path),
+            }
+        }
         _ => {
-            println!("Invalid sub command!\n\nAll valid sub commands:\n\t run [PATH]")
+            println!("Invalid sub command!\n\nAll valid sub commands:\n\t run [PATH]\n\t compile [PATH]")
         },
     }
 }
