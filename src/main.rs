a23cdfbed4b322b2622eb657eb01fc2a6a7da45b
Kennet Mathisen <kennetam02@gmail.com>
2022-06-20 15:50:28 +0200

    fixed problem with file extentions

--- src/main.rs
+++ src/main.rs
@@ -9,53 +9,46 @@ fn main() {
         Some(v) => v.to_owned(),
         None => String::from(""),
     };
+
+    let path = &args[2];
+
+    let file_extension = {
+        let dot_index = path.rfind(".").unwrap();
+        &path[dot_index..]
+    };
+
+    if file_extension != ".bf" && file_extension != ".rbf" {
+        println!("ERROR: Invalid file extension");
+        std::process::exit(0)
+    }
+
+    let program = match fs::read_to_string(path) {
+        Ok(x) => x,
+        Err(_) => {
+            println!("ERROR: Invalid path: {} does not exists", path);
+            std::process::exit(0)
+        }
+    };
+
     match sub_command.as_str() {
         "run" | "r" => {
-            let path = &args[2];
-            let file_extension = {
-                let dot_index = path.find(".").unwrap();
-                &path[dot_index..]
+            let prg = if file_extension == ".bf" {
+                brain_rust::compiler::compile(&program)
+            } else {
+                brain_rust::parser::parse(&program)
             };
-            match file_extension {
-                ".bf" => {
-                    let program_file = fs::read_to_string(path);
-                    match program_file {
-                        Ok(prog) => {
-                            let prg = brain_rust::compiler::compile(&prog);
-                            brain_rust::runtime::Runtime::new(prg).run()
-                        }
-                        Err(_) => println!("ERROR: Invalid path: {}", path),
-                    }
-                }
-                ".rbf" => {
-                    let program_file = fs::read_to_string(path);
-                    match program_file {
-                        Ok(prog) => {
-                            let prg = brain_rust::parser::parse(&prog);
-                            brain_rust::runtime::Runtime::new(prg).run()
-                        }
-                        Err(_) => println!("ERROR: Invalid path: {}", path),
-                    }
-                }
-                _ => println!("ERROR: Invalid file extension"),
-            }
+
+            brain_rust::runtime::Runtime::new(prg).run()
         }
         "compile" | "c" => {
-            let path = &args[2];
-            let program_file = fs::read_to_string(path);
-            match program_file {
-                Ok(prog) => {
-                    let file_name = &path[0..path.len() - 3];
-                    let compiled_prog = brain_rust::compiler::compile(&prog);
-                    let mut output = File::create(format!("{}.rbf", file_name))
-                        .expect("Was unable to create file.");
-                    for i in &compiled_prog {
-                        output
-                            .write_all(format!("{}", i).as_bytes())
-                            .expect("Was unable to write data to file.");
-                    }
-                }
-                Err(_) => println!("ERROR: Invalid path: {}", path),
+            let file_name = &path[0..path.rfind(".").unwrap()];
+            let compiled_program = brain_rust::compiler::compile(&program);
+            let mut output =
+                File::create(format!("{}.rbf", file_name)).expect("Was unable to create file.");
+            for i in &compiled_program {
+                output
+                    .write_all(format!("{}", i).as_bytes())
+                    .expect("Was unable to write data to file.");
             }
         }
         _ => {
